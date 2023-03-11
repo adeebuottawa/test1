ceff9140b80a4ebfb65393b758b2112b625a0807
adeebuottawa <126712149+adeebuottawa@users.noreply.github.com>
2023-03-11 15:10:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def predict_disease(data: patientData):
 
 # 6. Run the API with uvicorn. Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='local host', port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
 
 # To run the API, first change the directory to the current project then use the command,
 # uvicorn app:app --reload
